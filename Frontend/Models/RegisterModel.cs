00ce3ca9c71b35c613293673cfae285d88a1928c
MaxAlm <max.almroth95@gmail.com>
2022-11-23 11:23:23 +0100

    Lagt till SASS, fixat en navigation meny samt sida för inloggning och registrering.

---
+++ Frontend/Models/RegisterModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Frontend.Models
+{
+    public class RegisterModel
+    {
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Username { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? RepeatPassword { get; set; }
+    }
+}
