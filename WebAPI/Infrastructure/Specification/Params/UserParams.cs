91e534f94eab0a36db25761e45b34f5f603fc637
MaxAlm <max.almroth95@gmail.com>
2022-11-24 13:19:40 +0100

    Lagt till så alla Controllers ärver Controller klassen, fixat så att alla endpoints returnerar passande statuskoder, lagt till ytterligare logik för specification pattern samt lagt till en endpoint för GetAllUsers med fungerande specification pattern. Lagt till en proxy för web api:n.

---
+++ WebAPI/Infrastructure/Specification/Params/UserParams.cs
@@ -0,0 +1,21 @@
+﻿namespace WebAPI.Infrastructure.Specification.Params
+{
+    public class UserParams
+    {
+        private string? _username;
+
+        private string? _sort;
+
+        public string? Username
+        {
+            get { return _username; }
+            set { _username = (value != null ? value.ToLower() : value); }
+        }
+
+        public string? Sort
+        {
+            get { return _sort; }
+            set { _sort = (value != null ? value.ToLower() : value); }
+        }
+    }
+}
---
+++ WebAPI/Infrastructure/Specification/UserSpecification.cs
@@ -0,0 +1,27 @@
+﻿using WebAPI.Infrastructure.Specification.Params;
+using WebAPI.Models;
+using WebAPI.Specification;
+
+namespace WebAPI.Infrastructure.Specification
+{
+    public class UserSpecification : BaseSpecification<User>
+    {
+        public UserSpecification(UserParams userParams) : base(x => 
+            (!string.IsNullOrEmpty(userParams.Username) ? x.Username.ToLower().Contains(userParams.Username) : true))
+        {
+            if (!string.IsNullOrEmpty(userParams.Sort))
+            {
+                switch (userParams.Sort)
+                {
+                    case "registeredascending":
+                        SortMethod(x => x.DateRegistered);
+                        break;
+
+                    case "registereddescending":
+                        SortDescendingMethod(x => x.DateRegistered);
+                        break;
+                }
+            }
+        }
+    }
+}
