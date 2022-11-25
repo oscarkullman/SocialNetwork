79c037a6233d744f5c5136492203261466aebdcb
Roman Parker <roman@parker.pm>
2022-11-25 12:03:32 +0100

    added token generation on login

--- WebAPI/Controllers/AccountController.cs
+++ WebAPI/Controllers/AccountController.cs
@@ -1,8 +1,13 @@
 ﻿using Frontend.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using SocialNetwork.Classes.Account;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using WebAPI.Infrastructure.Services;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
@@ -11,26 +16,73 @@ namespace WebAPI.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _siginManager;
+        private readonly SignInManager<IdentityUser> _signinManager;
         private readonly IUserService _userService;
-
+        private readonly IConfiguration _config;
         public AccountController(
             UserManager<IdentityUser> userManager,
             SignInManager<IdentityUser> siginManager,
-            IUserService userService)
+            IUserService userService,
+            IConfiguration config)
         {
             _userManager = userManager;
-            _siginManager = siginManager;
+            _signinManager = siginManager;
             _userService = userService;
+            _config = config;
         }
 
         [HttpPost("Login")]
-        public async Task<ActionResult> Login([FromBody]LoginModel loginModel)
+        public async Task<ActionResult> Login([FromBody] LoginModel loginModel)
         {
-            // Logik för inloggning
-            return Ok();
+            var user = await Authenticate(loginModel);
+            if (user != null)
+            {
+                var token = Generate(user);
+                return Ok(token);
+
+            }
+            return NotFound("User not found");
         }
-        
+
+        private string Generate(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                //new Claim(ClaimTypes.Email, user.Email ),
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: null,
+                audience: null,
+                claims: claims,
+                expires: DateTime.Now.AddDays(10),
+                signingCredentials: credentials
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private async Task<User>? Authenticate(LoginModel loginModel)
+        {
+            var user = await _userManager.FindByNameAsync(loginModel.Username);
+            var validCredentials = await _signinManager.UserManager.CheckPasswordAsync(user, loginModel.Password);
+
+            //var users =  await _userService.GetAllUsers(new UserSpecification(new UserParams()));
+            //var currentUser = users.FirstOrDefault(o => o.Username.ToLower() == loginModel.Username.ToLower());
+            if (validCredentials)
+            {
+                return new User
+                {
+                    Username = user.UserName
+                };
+            }
+
+            return null;
+        }
+
         [HttpPost("Register")]
         public async Task<ActionResult> Register([FromBody]RegisterModel registerModel)
         {
@@ -47,7 +99,7 @@ namespace WebAPI.Controllers
             if (newUser.Succeeded)
             {
                 await _userService.AddNewUser(registerModel);
-                await _siginManager.SignInAsync(user, isPersistent: false);
+                await _signinManager.SignInAsync(user, isPersistent: false);
                 return Ok($"Successfully registered new user with username {user.UserName}");
             }
 
