5e1cfdf4eb33814f449b1793372af5cea157eb6d
MaxAlm <max.almroth95@gmail.com>
2022-11-26 21:00:32 +0100

    Lagt till två nya klasser med metoder för validering av registreringsformulär och lösenord. Har även skrivit tester för dessa.

---
+++ SocialNetwork.Classes/Frontend/PasswordValidationResult.cs
@@ -0,0 +1,15 @@
+﻿namespace SocialNetwork.Classes.Frontend
+{
+    public class PasswordValidationResult
+    {
+        public PasswordValidationResult(string message = "", bool success = false)
+        {
+            Message = message;
+            Success = success;
+        }
+        
+        public string? Message { get; set; }
+
+        public bool Success { get; set; }
+    }
+}
