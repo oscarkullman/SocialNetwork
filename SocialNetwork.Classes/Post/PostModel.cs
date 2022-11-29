ecb7a138db4026cbe98e8ce854fa98969ed959a6
Roman Parker <roman@parker.pm>
2022-11-29 13:30:44 +0100

    added new postrepository

---
+++ SocialNetwork.Classes/Post/PostModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Classes.Post
+{
+    public class PostModel
+    {
+        public string? Username { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Content { get; set; }
+    }
+}
---
+++ WebAPI/Infrastructure/Repositories/IPostRepository.cs
@@ -0,0 +1,9 @@
+﻿using WebAPI.Entities;
+using WebAPI.Repositories;
+
+namespace WebAPI.Infrastructure.Repositories
+{
+    public interface IPostRepository : IRepository<Post>
+    {
+    }
+}
