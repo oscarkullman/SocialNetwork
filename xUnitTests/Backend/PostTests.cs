c3967dd63ec7b83e9f4c3642e89fe422ec58d108
Roman <Roman@DESKTOP-8ACHKI7>
2022-12-01 10:23:34 +0100

    added poststests

---
+++ xUnitTests/Backend/PostTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Entities;
+
+namespace xUnitTests.Backend
+{
+    public class PostTests
+    {
+
+        private List<Post>  _posts = new List<Post>{
+            new Post{
+                PostId = Guid.NewGuid(),
+
+                Username = "Rompa23",
+                FirstName = "Roman",
+
+                LastName = "Parker",
+
+                Content = "My first post, the wheater is very nice today! Have a good day my followers!",
+
+                DateCreated = DateTime.Now,
+            },
+            new Post{
+                PostId = Guid.NewGuid(),
+
+                Username = "masken1",
+                FirstName = "Max",
+
+                LastName = "Almroth",
+
+                Content = "My first post, the wheater is very nice today! Have a good day my followers!",
+
+                DateCreated = DateTime.Now,
+            },
+            new Post{
+                PostId = Guid.NewGuid(),
+
+                Username = "Kullen12",
+                FirstName = "Oscar",
+
+                LastName = "Kullman",
+
+                Content = "My first post, the wheater is very nice today! Have a good day my followers!",
+
+                DateCreated = DateTime.Now,
+            },
+
+        };
+    }
+}
